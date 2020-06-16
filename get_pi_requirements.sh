dd4d18d7f7042bff1e9348d9d2f54df195187e8f
madesv <adibfarrasy@gmail.com>
2020-06-16 15:52:33 +0800

    Update get_pi_requirements.sh

--- get_pi_requirements.sh
+++ get_pi_requirements.sh
@@ -8,25 +8,12 @@ sudo apt-get -y install libxvidcore-dev libx264-dev
 sudo apt-get -y install qt4-dev-tools libatlas-base-dev
 
 # Need to get an older version of OpenCV because version 4 has errors
-pip3 install opencv-python==3.4.6.27
+pip install opencv-python
+pip3 install opencv-python
+#pip3 install opencv-python==3.4.6.27
 
 # Get packages required for TensorFlow
 # Using the tflite_runtime packages available at https://www.tensorflow.org/lite/guide/python
 # Will change to just 'pip3 install tensorflow' once newer versions of TF are added to piwheels
 
-#pip3 install tensorflow
-
-version=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
-
-if [ $version == "3.8" ]; then
-pip3 install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow_cpu-2.2.0-cp38-cp38-manylinux2010_x86_64.whl
-fi
-
-if [ $version == "3.7" ]; then
-pip3 install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow_cpu-2.2.0-cp37-cp37m-manylinux2010_x86_64.whl
-fi
-
-if [ $version == "3.5" ]; then
-pip3 install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow_cpu-2.2.0-cp35-cp35m-manylinux2010_x86_64.whl
-fi
-
+pip3 install https://storage.googleapis.com/tensorflow/raspberrypi/tensorflow-2.2.0-cp35-none-linux_armv7l.whl
